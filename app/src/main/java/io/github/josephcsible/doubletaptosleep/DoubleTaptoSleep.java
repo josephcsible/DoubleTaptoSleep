71a78f424e67edf5187ff74a35758b5040e98c04
Joseph C. Sible <josephcsible@users.noreply.github.com>
2022-06-21 21:42:38 -0400

    Add GPLv3 or later license header

--- app/src/main/java/io/github/josephcsible/doubletaptosleep/DoubleTaptoSleep.java
+++ app/src/main/java/io/github/josephcsible/doubletaptosleep/DoubleTaptoSleep.java
@@ -1,3 +1,21 @@
+/*
+An Xposed module that turns off the display when you double-tap on the status bar or lockscreen
+Copyright (C) 2022  Joseph C. Sible
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package io.github.josephcsible.doubletaptosleep;
 
 import android.content.Context;
